75847c53ea83ffde76d16c21088c043d7d4fefd5
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-02-26 15:24:10 +0000

    refactored campaigns controller

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,29 +1,38 @@
 class CampaignsController < ApplicationController
+
   def index
-    @votes = Vote.all
+    votes = Vote.all
     @campaigns = []
-
-    @votes.map do |vote|
-      if !@campaigns.include?(vote.campaign)
-        @campaigns << vote.campaign
-      end
-    end
-
+    votes.map { |vote| @campaigns << vote.campaign if !@campaigns.include?(vote.campaign)}
   end
 
   def show
     @campaign_votes = Vote.where(campaign: params[:id], validity: 'during')
-    @choices = []
-    @campaign_votes.map do |vote|
-      if !@choices.include?(vote.choice)
-        @choices << vote.choice
-      end
+    @choice_info = derive_data_to_show_from @campaign_votes
+  end
+
+
+  private
+
+  def derive_data_to_show_from campaign_votes
+    choices = get_possible_choices(campaign_votes)
+    create_info_for(choices, campaign_votes)
+  end
+
+  def get_possible_choices campaign_votes
+    choices = []
+    campaign_votes.map { |vote| choices << vote.choice if !choices.include?(vote.choice)}
+    choices
+  end
+
+  def create_info_for (choices, campaign_votes)
+    choice_info = []
+    for name in choices do
+      votes_per_choice = campaign_votes.select { |vote| vote.choice == name }
+      percentage = (votes_per_choice.count * 100) / campaign_votes.count
+      choice_info << {name: name, votes: votes_per_choice.count, percentage: percentage }
     end
-    @choice_info = []
-    for @name in @choices do
-      @votes = @campaign_votes.select {|vote| vote.choice == @name}
-      @choice_info << {name: @name, votes: @votes.count}
-    end
+    choice_info
   end
 
 end
